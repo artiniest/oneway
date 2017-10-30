59356be1dbcebff9ffbda085384603b9f304f3bd
Saara Korhonen <saara.korhonen@courses.tamk.fi>
2017-10-30 12:33:31 +0200

    Esteitä spawnaa

---
+++ One Way/Assets/Scripts/SceneManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour 
+{
+	public GameObject[] obstacles; //Array esteille, tästä spawnataan
+	public GameObject[] spawnedObstacles; //Array scenessä aktiivisille esteille
+	public GameObject[] tracks; //Array radoille
+	public float moveSpeed = 10f; //Liikkumisnopeus esteille
+
+	void Start ()
+	{
+		InvokeRepeating ("Spawn", 1, 2); //Kuinka usein Spawn-funktio käynnistetään
+	}
+
+	void Update ()
+	{
+		spawnedObstacles = GameObject.FindGameObjectsWithTag ("Obstacle"); //Etsii objekteja Obstacle-tagilla ja lisää ne listaan
+	}
+
+	void FixedUpdate()
+	{
+		foreach (GameObject obj in spawnedObstacles) 
+		{
+			obj.gameObject.transform.Translate (new Vector2 (transform.position.x, moveSpeed * Time.deltaTime));
+		}
+	}
+
+	void Spawn ()
+	{
+		Instantiate (obstacles[0], new Vector2(tracks[Random.Range(0, tracks.Length)].transform.position.x, -15),Quaternion.identity); //Spawnataan obstacles-listasta indexin mukainen objekti, randomille radalle.
+	}
+}

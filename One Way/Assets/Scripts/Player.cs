066e48b073014530b46c8be317340444990554fe
Saara Korhonen <korhonensaara@hotmail.com>
2017-10-31 13:13:07 +0200

    Naama

--- One Way/Assets/Scripts/Player.cs
+++ One Way/Assets/Scripts/Player.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class Player : MonoBehaviour 
 {
-	//Älä koske tämän yläpuolelle mihinkään!!!
+    //Älä koske tämän yläpuolelle mihinkään!!!
 
+    public GameObject face; //Naaama
 	public GameObject[] tracks; //Luodaan GameObject-lista, johon lisätään Inspectorista "radat"
 	public GameObject kamera;
 	public int currentTrack; //0 = kaukainen oikea, 1 = oikea, 2 = keskimmäinen, 3 = vasen, 4 = kaukainen vasen
@@ -60,6 +61,7 @@ public class Player : MonoBehaviour
             transform.Translate(new Vector2(transform.position.x, +2)); //Siirretään pelaajaa yksi yksikkö ylemmäs = lähemmäs lonkeroita
             kamera.transform.Translate(new Vector2(kamera.transform.position.x, +2)); //Siirretään kameraa myös ylöspäin
             Destroy(other.GetComponent<Collider2D>()); //Tuhotaan esteen collider, ettei tule ongelmia :)
+            face.SetActive(true);
         }
 
         if (other.tag == "Powerup") //Jos toisen objektin tagi on "Powerup"
@@ -74,4 +76,9 @@ public class Player : MonoBehaviour
             Application.LoadLevel(0); //Ladataan 0-scene (nykyinen scene siis, atm) uudelleen.
         }
 	}
+
+    void OnTriggerExit2D (Collider2D other)
+    {
+        face.SetActive(false);
+    }
 }

96009ee2e007e761efb1022f29ecc17f92977dbc
Saara Korhonen <saara.korhonen@courses.tamk.fi>
2017-11-02 13:58:18 +0200

    no message

--- One Way/Assets/Scripts/Control.cs
+++ One Way/Assets/Scripts/Control.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Control : MonoBehaviour 
 {
 	public int levelToLoad = 0;
+	public GameObject img;
+	bool isEnabled = false;
 
 	void Update () 
 	{
@@ -12,5 +15,30 @@ public class Control : MonoBehaviour
 		{
 			Application.LoadLevel (levelToLoad);
 		}
+
+		if (isEnabled == false) 
+		{
+			img.gameObject.SetActive (false);
+		}
+
+		if (isEnabled == true) 
+		{
+			img.gameObject.SetActive (true);
+		}
+	}
+
+	public void OnClick ()
+	{
+		Application.LoadLevel (levelToLoad);
+	}
+
+	public void OnClick2 ()
+	{
+		isEnabled = true;
+	}
+
+	public void OnClick3()
+	{
+		isEnabled = false;
 	}
 }
